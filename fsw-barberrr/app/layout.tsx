1c66e82b656445beb7e6907902e6aed15e37565d
Luiz Henrique <https://github.com/luizmorais12/Rockets.git>
2024-08-12 15:41:34 -0300

    feat: erros

--- fsw-barberrr/app/layout.tsx
+++ fsw-barberrr/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "./_components/footer";
+import AuthProvider from "./_providers/auth";
+import { Toaster } from "./_components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} dark`}>
+        <AuthProvider>
+          <div className="flex-1">{children}</div>
+          <Toaster />
+          <Footer />
+        </AuthProvider>
+      </body>
     </html>
   );
 }
